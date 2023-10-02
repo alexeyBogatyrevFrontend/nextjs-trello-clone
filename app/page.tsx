0101e2332315fe467e2269735b1316516fada05e
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-10-02 15:40:03 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const page = () => {
+	return (
+		<main>
+			{/* Header */}
+			<Header />
+			{/* Board */}
+			<h1>Trello 2.0 AI Clone</h1>
+		</main>
+	)
+}
+
+export default page
